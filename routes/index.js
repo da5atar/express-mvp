4daf4007c34cfac038001712bad81f4dddaef674
da5atar <mass.sow@gmail.com>
2019-02-17 18:14:04 +0000

    Add controllers logic

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+let index = require('../controllers/index');
+
 /* GET home page. */
 // TODO 1: Separate the route handler logic
-router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Express'
-  });
-});
+router.get('/', index.index);
 
 module.exports = router;
