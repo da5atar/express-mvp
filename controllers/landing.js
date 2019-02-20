7495c050043a524f9e59d7bb68f3d08292de3ee8
da5atar <mass.sow@gmail.com>
2019-02-20 19:07:50 +0000

    add POST route to save emails collected on landing page back to database

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,3 +1,6 @@
+// TODO 11: Implement POST route to save Emails
+const models = require('../models');
+
 // TODO 3: Landing page controller
 exports.get_landing = function (req, res, next) {
   res.render('landing', {
@@ -7,6 +10,10 @@ exports.get_landing = function (req, res, next) {
 
 // TODO 6: Landing page form controller
 exports.submit_lead = function (req, res, next) {
-  console.log("lead email:", req.body.lead_email);
-  res.redirect('/');
+  // console.log("lead email:", req.body.lead_email);
+  return models.Lead.create({
+    email: req.body.lead_email
+  }).then(lead => {
+    res.redirect('/');
+  })
 }
