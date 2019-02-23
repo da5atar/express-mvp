7817cc1283e58936866a2c61ef6c2888ac080b5b
da5atar <mass.sow@gmail.com>
2019-02-23 18:55:15 +0000

    Add lead edit routes

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,17 @@ router.get('/', landing.get_landing);
 
 // TODO 5: Develop first POST route for leads submissions
 router.post('/', landing.submit_lead);
-// TODO 12: Implement CRUD pattern on leads (listing, editing, updating and deleting)
 
+// TODO 12: Implement CRUD pattern on leads (listing, editing, updating and deleting)
 // CRUD: listing of leads route
 router.get('/leads', landing.show_leads);
 
 // CRUD: listing details of leads
 router.get('/lead/:lead_id', landing.show_lead);
+
+// TODO 14: editing leads
+// CRUD: editing individual lead
+router.get('/lead/:lead_id/edit', landing.show_edit_lead);
+router.post('/lead/:lead_id/edit', landing.edit_lead);
 
 module.exports = router;
